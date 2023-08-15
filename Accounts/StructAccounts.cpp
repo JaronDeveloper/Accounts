4bde725fec6aee46d6c62be49841e74fba0673d8
Prodject <Liter.Hari@gmail.com>
2023-08-15 20:43:29 +0200

    Accounts

--- Accounts/StructAccounts.cpp
+++ Accounts/StructAccounts.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <Windows.h>
 
 struct BankAccount {
 	int numberAccount;
@@ -7,17 +8,26 @@ struct BankAccount {
 	int newBalance;
 };
 
+int changeballance(BankAccount bank) {	
+	int change = (bank.balance + bank.newBalance) / 2;
+	return change;
+}
+
+
 int main() {
 	BankAccount bank;
 
 	setlocale(LC_ALL, "Russian");
 	std::cout << "¬ведите номер счЄта: ";
 	std::cin >> bank.numberAccount;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	std::cout << "¬ведите им€ владельца: ";
 	std::cin >> bank.OwnersName;
 	std::cout << "¬ведите баланс: ";
 	std::cin >> bank.balance;
 	std::cout << "¬ведите новый баланс: ";
 	std::cin >> bank.newBalance;
-	std::cout << "¬аш счЄт: " << bank.OwnersName << "," << bank.numberAccount << "," << bank.newBalance << std::endl;
+	int newBalanceBanckAccount = changeballance(bank);
+	std::cout << "¬аш счЄт: " << bank.OwnersName << "," << bank.numberAccount << "," << newBalanceBanckAccount << std::endl;
 }
